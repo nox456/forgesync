bec90e1e229feabce8509051a76b8401177ada71
Gabriel Vieira <nox030705@gmail.com>
2026-05-27 19:11:58 -0400

    refactor(notion): moved UpsertStory to upsert.go

---
+++ internal/notion/upsert.go
@@ -0,0 +1,153 @@
+package notion
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"slices"
+
+	"github.com/nox456/forgesync/internal/github"
+)
+
+type UpsertResult struct {
+	Created   bool
+	Updated   bool
+	Unchanged bool
+}
+
+func (c *Client) UpsertStory(storyInput StoryInput, issue github.Issue, isDryRun bool) (*UpsertResult, error) {
+	existingStory, err := c.FindStoryByIssue(issue)
+
+	if err != nil {
+		return nil, err
+	}
+
+	labels := make([]NamedOption, len(storyInput.Labels))
+	for i, l := range storyInput.Labels {
+		labels[i] = NamedOption{Name: l}
+	}
+
+	baseProps := StoryProperties{
+		Name: &TitleInputProperty{
+			Title: []RichTextInput{
+				{Text: TextContent{Content: storyInput.Name}},
+			},
+		},
+		Status: &StatusProperty{Status: NamedOption{Name: storyInput.Status}},
+		Labels: &MultiSelectProperty{MultiSelect: labels},
+		LastWorkedAt: &DateProperty{
+			Date: DateValue{
+				Start: storyInput.LastWorkedAt,
+			},
+		},
+	}
+
+	if storyInput.FinishedDate != "" {
+		baseProps.FinishedDate = &DateProperty{
+			Date: DateValue{
+				Start: storyInput.FinishedDate,
+			},
+		}
+	}
+
+	if existingStory == nil {
+
+		if isDryRun {
+			return &UpsertResult{
+				Created: true,
+			}, nil
+		}
+
+		url := fmt.Sprintf("%s/pages", notionBaseUrl)
+
+		createProps := baseProps
+		createProps.Project = &RelationProperty{
+			Relation: []RelationRef{{ID: storyInput.Project}},
+		}
+		createProps.Issue = &NumberProperty{Number: issue.Number}
+		createProps.URL = &URLProperty{URL: storyInput.Url}
+
+		createPayload := &StoryCreatePayload{
+			Parent:     PageParent{DataSourceID: c.StoriesSourceId},
+			Properties: createProps,
+		}
+
+		body, err := json.Marshal(createPayload)
+		if err != nil {
+			return nil, err
+		}
+
+		req, _ := http.NewRequest("POST", url, bytes.NewReader(body))
+
+		req.Header.Add("Notion-Version", notionApiVersion)
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.Token))
+		req.Header.Add("Content-Type", "application/json")
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode >= 400 {
+			b, _ := io.ReadAll(res.Body)
+			return nil, fmt.Errorf("notion api %d: %s", res.StatusCode, string(b))
+		}
+
+		return &UpsertResult{
+			Created: true,
+		}, nil
+	} else {
+
+		hasSameName := existingStory.Name == storyInput.Name
+		hasSameStatus := existingStory.Status == storyInput.Status
+		hasSameLabels := slices.Equal(existingStory.Labels, storyInput.Labels)
+		hasSameLastWorkedAt := existingStory.LastWorkedAt == storyInput.LastWorkedAt
+
+		if hasSameName && hasSameStatus && hasSameLabels && hasSameLastWorkedAt {
+			return &UpsertResult{
+				Unchanged: true,
+			}, nil
+		}
+
+		if isDryRun {
+			return &UpsertResult{
+				Updated: true,
+			}, nil
+		}
+
+		url := fmt.Sprintf("%s/pages/%s", notionBaseUrl, existingStory.PageID)
+
+		updatePayload := &StoryUpdatePayload{
+			Properties: baseProps,
+		}
+
+		body, err := json.Marshal(updatePayload)
+		if err != nil {
+			return nil, err
+		}
+
+		req, _ := http.NewRequest("PATCH", url, bytes.NewReader(body))
+
+		req.Header.Add("Notion-Version", notionApiVersion)
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.Token))
+		req.Header.Add("Content-Type", "application/json")
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode >= 400 {
+			b, _ := io.ReadAll(res.Body)
+			return nil, fmt.Errorf("notion api %d: %s", res.StatusCode, string(b))
+		}
+
+		return &UpsertResult{
+			Updated: true,
+		}, nil
+	}
+}
